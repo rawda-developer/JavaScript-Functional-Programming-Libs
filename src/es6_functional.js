350874b8360d8a645f973e5615c65f74385fd9d1
Rawda Yasser <rawda.developer@gmail.com>
2022-05-08 20:01:23 +0200

    Feat: Add tellType function to tell the type of data for its argument

--- src/es6_functional.js
+++ src/es6_functional.js
@@ -1,6 +1,9 @@
 const forEach = (arr, fn) => {
-    for (let i = 0; i < arr.length; i++) {
-        fn(arr[i]);
-    }
-}
-export default forEach;
+  for (let i = 0; i < arr.length; i++) {
+    fn(arr[i]);
+  }
+};
+const tellType = (data) => {
+     console.log("The type of data is " + typeof data);
+};
+export { forEach, tellType };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import forEach from "./es6_functional";
+import {forEach, tellType }from "./es6_functional";
 forEach([1, 2, 3], (data)=>console.log(data));
+console.log("\n");
+console.log(typeof forEach);
+tellType(1);
+tellType(forEach);
